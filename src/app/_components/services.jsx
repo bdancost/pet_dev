cb95c92109ceaaef4da7b8334fa084df2af9f254
Daniel Fernandes Costa <pr.danfc88@gmail.com>
2025-02-18 18:38:11 -0300

    Editando component testimonials

--- src/app/_components/services.jsx
+++ src/app/_components/services.jsx
@@ -64,6 +64,14 @@ export function Services() {
     },
   });
 
+  function scrollPrev() {
+    emblaApi?.scrollPrev();
+  }
+
+  function scrollNext() {
+    emblaApi?.scrollNext();
+  }
+
   return (
     <section className="bg-white py-16">
       <div className="container mx-auto px-4">
@@ -97,12 +105,34 @@ export function Services() {
                         <Clock className="w-4 h-4" />
                         <span>{item.duration}</span>
                       </div>
+
+                      <a
+                        href="#"
+                        className="flex items-center gap-2 text-sm justify-center hover:bg-red-500 px-4 py-1 rounded-md duration-300"
+                      >
+                        <WhatsappLogo className="w-5 h-5" />
+                        Entrar em contato
+                      </a>
                     </div>
                   </article>
                 </div>
               ))}
             </div>
           </div>
+
+          <button
+            onClick={scrollPrev}
+            className="bg-white flex items-center justify-center rounded-full shadow-lg w-10 h-10 absolute left-0 top-1/2 -translate-y-1/2 z-10"
+          >
+            <ChevronLeft className="w-6 h-6 text-gray-600" />
+          </button>
+
+          <button
+            onClick={scrollNext}
+            className="bg-white flex items-center justify-center rounded-full shadow-lg w-10 h-10 absolute -right-2 top-1/2 -translate-y-1/2 z-10"
+          >
+            <ChevronRight className="w-6 h-6 text-gray-600" />
+          </button>
         </div>
       </div>
     </section>
