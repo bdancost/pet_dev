d2e1344f49171eea93ce7146d408c474cafd8a6d
Daniel Fernandes Costa <pr.danfc88@gmail.com>
2025-02-19 06:35:06 -0300

    Ajustando o footer

---
+++ src/app/_components/footer.tsx
@@ -0,0 +1,57 @@
+import golden from "../../../public/image/golden.png";
+import royal from "../../../public/image/royal.png";
+import primier from "../../../public/image/primier.png";
+import whiskas from "../../../public/image/whiskas.png";
+import natural from "../../../public/image/natural.png";
+import Image from "next/image";
+
+const brands = [
+  { name: "Royal Canin", logo: royal },
+  { name: "Golden", logo: golden },
+  { name: "Primier", logo: primier },
+  { name: "Formula Natural", logo: natural },
+  { name: "Whiskas", logo: whiskas },
+  { name: "Golden", logo: golden },
+];
+
+export function Footer() {
+  return (
+    <section className="bg-[#E84c3d] py-16 text-white">
+      <div className="container mx-auto px-4">
+        <div className="border-b border-white/20 pb-8">
+          <h4 className="text-3xl font-semibold mb-8 text-center">
+            Marcas que trabalhamos
+          </h4>
+          <div className="grid grid-cols-2 lg:grid-cols-6 gap-8">
+            {brands.map((item, index) => (
+              <div
+                key={index}
+                className="bg-white p-4 rounded-lg flex items-center justify-center"
+              >
+                <Image
+                  src={item.logo}
+                  alt={item.name}
+                  width={100}
+                  height={50}
+                  quality={100}
+                  style={{
+                    width: "auto",
+                    height: "auto",
+                  }}
+                  className="object.contain"
+                />
+              </div>
+            ))}
+          </div>
+        </div>
+
+        <footer className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 mb-12 mt-5">
+          <div>
+            <h3 className="text-2xl font-semibold mb-2">Pet Shop Dev</h3>
+            <p>Cuidando do seu melhor amigo com amor e dedicação.</p>
+          </div>
+        </footer>
+      </div>
+    </section>
+  );
+}
