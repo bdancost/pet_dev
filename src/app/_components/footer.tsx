167f12d3b69abf0546c6b22031ebc2babf476c24
Daniel Fernandes Costa <pr.danfc88@gmail.com>
2025-02-20 13:03:11 -0300

    Ajustando animaçoes no página

--- src/app/_components/footer.tsx
+++ src/app/_components/footer.tsx
@@ -26,13 +26,16 @@ export function Footer() {
     <section className="bg-[#E84c3d] py-16 text-white">
       <div className="container mx-auto px-4">
         <div className="border-b border-white/20 pb-8">
-          <h4 className="text-3xl font-semibold mb-8 text-center">
+          <h4
+            data-aos="fade-down"
+            className="text-3xl font-semibold mb-8 text-center"
+          >
             Marcas que trabalhamos
           </h4>
           <div
             data-aos="flip-left"
             data-aos-easing="ease-out-cubic"
-            data-aos-duration="2000"
+            data-aos-duration="3000"
             className="grid grid-cols-2 lg:grid-cols-6 gap-8"
           >
             {brands.map((item, index) => (
@@ -58,7 +61,7 @@ export function Footer() {
         </div>
 
         <footer className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 mb-12 mt-5">
-          <div>
+          <div data-aos="fade-up-right">
             <h3 className="text-2xl font-semibold mb-2">Pet Shop Dev</h3>
             <p className="mb-4">
               Cuidando do seu melhor amigo com amor e dedicação.
@@ -68,14 +71,14 @@ export function Footer() {
             </a>
           </div>
 
-          <div>
+          <div data-aos="fade-up">
             <h3 className="text-2xl font-semibold mb-2">Contatos</h3>
             <p>Email: pet@shopdev.com</p>
             <p>Telefone: (11) 99999-5676</p>
             <p>Rua: Av. das Nações Unidas, 123 | SP</p>
           </div>
 
-          <div>
+          <div data-aos="fade-up-left">
             <h3 className="text-2xl font-semibold mb-2">Redes Sociais</h3>
             <div className="flex gap-4">
               <a href="#" target="_blank">
